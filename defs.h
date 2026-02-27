59aad5fe0b47399b7a60461c38a2fe93f6631827
Matheus Grossi <matheusgrossi2000@gmail.com>
2026-02-27 14:58:38 -0300

    Adição dos arquvos base de projeto

---
+++ defs.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   defs.h
+ * Author: matheus
+ *
+ * Created on 27 de Fevereiro de 2026, 14:57
+ */
+
+#ifndef DEFS_H
+#define	DEFS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DEFS_H */
+
---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: matheus
+ *
+ * Created on 27 de Fevereiro de 2026, 14:57
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
